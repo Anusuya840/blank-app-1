3c8a03c5bb759ce081e6300c3d200f561b1edaee
Anusuya840 <anukesavapp29@gmail.com>
2025-02-22 19:17:13 +0000

    git add requirements.txt git commit -m "Added requirements.txt" git push origin main

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,36 @@
 import streamlit as st
+import mysql.connector
+import pandas as pd
+import requests
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title('📚 BookScape Explorer')
+st.header('Clickhere')
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host=st.secrets["DB_HOST"],
+        port=int(st.secrets["DB_PORT"]), 
+        user=st.secrets["DB_USER"],
+        password=st.secrets["DB_PASSWORD"],
+        database=st.secrets["DB_NAME"]
+    )
+
+def fetch_books_from_db():
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM books LIMIT 1000") 
+    books = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return books
+
+
+if st.button("Fetch Books Data"):
+    books_data = fetch_books_from_db()
+    
+    if books_data:
+        df = pd.DataFrame(books_data)
+        st.success(f" Loaded {len(df)} books successfully!")
+        st.dataframe(df) 
+    else:
+        st.warning("⚠ No books found in the database!")
